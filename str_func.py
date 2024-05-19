2466df323a65bff247e98b2bf69c4462d95b7e42
Wetal12311 <margenshteynv@gmail.com>
2024-05-19 14:12:09 +0300

    add gitignore

--- str_func.py
+++ str_func.py
@@ -1,4 +1,10 @@
 def upper_func():
+    """делает все буквы заглавными"""
     user_str = input('Введите строку\n')
     print(user_str.upper())
-upper_func()
+
+def title_func():
+    """делает первые буквы заглавными"""
+    user_str = input('Введите строку\n')
+    print(user_str.title())
+
